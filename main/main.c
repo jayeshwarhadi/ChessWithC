8466e38a16a5e90d0b89e9ae845216914b3c5cfb
jayesh-warhadi <www.jayeshwarhadi0308@gmail.com>
2025-02-13 17:37:25 +0530

    main boilerplate

---
+++ main/main.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <conio.h> //for executables retaining output screen when program is running (temporary)
+#include "logic.h"
+#include "screen.h"
+#include <stdlib.h>
+#include <time.h>
+
+/*
+    compiler statement = gcc main.c logic.c screen.c -o app
+*/
